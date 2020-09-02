c654405ee9217a8bad635d46a56364cd42b98392
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-02 16:59:45 +0300

    spinners

--- src/app/pick-list/pick-list.component.ts
+++ src/app/pick-list/pick-list.component.ts
@@ -11,24 +11,33 @@ export class PickListComponent implements OnInit {
   ShipsTodayOther;
   ShipsLaterNextDay;
   ShipsLaterOther;
+  ShipTodayNextDayLoading=false;
+  ShipTodayOtherLoading=false;
+  ShipLaterNextDayLoading=false;
+  ShipsLaterOtherLoading=false;
 
   constructor(private orderService: OrderService) {}
 
   ngOnInit() {
+    this.ShipTodayNextDayLoading=true;
     this.orderService.getShipsTodayNexyDayCount().subscribe(data => {
       this.ShipsTodayNextDay = data.Count;
+      this.ShipTodayNextDayLoading=false;
     });
-
+     this.ShipTodayOtherLoading=true;
     this.orderService.getShipsTodayOtherCount().subscribe(data => {
       this.ShipsTodayOther = data.Count;
+      this.ShipTodayOtherLoading=false;
     });
-
+      this.ShipLaterNextDayLoading=true;
     this.orderService.getShipsLaterNextDayCount().subscribe(data => {
       this.ShipsLaterNextDay = data.Count;
+       this.ShipLaterNextDayLoading=false;
     });
-
+    this.ShipsLaterOtherLoading=true;
     this.orderService.getShipsLaterOtherCount().subscribe(data => {
       this.ShipsLaterOther = data.Count;
+          this.ShipsLaterOtherLoading=false;
     });
   }
 }
