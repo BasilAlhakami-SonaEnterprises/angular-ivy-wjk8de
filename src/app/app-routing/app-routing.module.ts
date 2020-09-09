216cd006784b819825bba28f5f5e6799dc380a1b
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-09 11:54:38 +0300

    quality control page created

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from "@angular/router"; // CLI imports router
 import { PickListComponent } from "../pick-list/pick-list.component";
 import { PickListItemComponent } from "../pick-list-item/pick-list-item.component";
 import { PrintLabelComponent } from "../print-label/print-label.component";
-
+import {QualityControlComponent} from "../quality-control/quality-control.component";
 
 const routes: Routes = [
   { path: "picking", component: PickListComponent },
   { path: "item-selection", component: PickListItemComponent },
   { path: "print-label", component: PrintLabelComponent },
+  {path:"quality-control",component:QualityControlComponent}
 
 ];
 
