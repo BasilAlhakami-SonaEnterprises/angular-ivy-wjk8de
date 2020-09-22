762f47c39a7a035bce7cc2b134ce4a76e01bc675
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-22 14:59:36 +0300

    1 file modified

--- src/app/quality-control/quality-control.component.ts
+++ src/app/quality-control/quality-control.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {LicensePlateService} from "../services/licenseplate.service";
-import {OrderService} from  "../services/order.service";
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-quality-control',
   templateUrl: './quality-control.component.html',
@@ -8,64 +8,24 @@ import {OrderService} from  "../services/order.service";
 })
 export class QualityControlComponent implements OnInit {
   
-  constructor(private licensePlateService:LicensePlateService,private orderService:OrderService) { }
-   licensePlateId;
-   poNumber;
-   trackingNumber;
-   order;
-   licensePlate;
-   creatingLicensePlate=false;
-   gettingLicensePlate=false;
-   gettingOrder=false;
-   addingOrderToLicensePlate=false;
+  constructor(private licensePlateService:LicensePlateService,private router:Router) { }
+   creatingAndPrintingLicensePlate=false;
+   licensePlateId
   ngOnInit() {
   }
-  createLicensePlate(){
-    this.creatingLicensePlate=true;
-    this.licensePlateService.createLicensePlate().subscribe(
-      (data)=>{
-      console.log(data);
-      this.licensePlateId=data;
-      this.creatingLicensePlate=false;
-      this.getLP();
+  createAndPrintLicensePlate(){
+    this.creatingAndPrintingLicensePlate=true;
+    this.licensePlateService.createLicensePlate().subscribe(()=>{
+      this.creatingAndPrintingLicensePlate=false;
     },
-    (err)=>{console.log(err)},
-    );
+    (err)=>{console.log(err)}
+    )
+  };
+  scanLicensePlate(){
+      this.router.navigate(['/scanScreen'],{queryParams:{licensePlateId:this.licensePlateId}});
   }
-  getLP(){
-   this.gettingLicensePlate=true;
  
-   this.licensePlateService.getLicensePlate(this.licensePlateId).subscribe(data=>{
-     this.licensePlate=data;
-     console.log(this.licensePlate);
-     this.gettingLicensePlate=false;
-     
-   },
-   (err)=>{console.log(err)}
-   );
   
-  }
-  getOrderandAddToLP(){
-    this.gettingOrder=true;
-    this.orderService.getByTrackingNumber(this.trackingNumber).subscribe(data=>{
-      this.order=data;
-      this.poNumber=this.order.PONumber;
-      this.gettingOrder=false;
-      this.addOrderToLicensePlate();
-    },
-    (err)=>{console.log(err)}
- 
-    );
-  }
-  addOrderToLicensePlate(){
-    this.addingOrderToLicensePlate=true;
-    this.licensePlateService.addOrderToLicensePlate(this.licensePlateId,this.poNumber).subscribe(()=>{
-      this.addingOrderToLicensePlate=false;
-       this.getLP();
-    },
-    (err)=>{console.log(err)}
-    );
-  }
 
 
 
