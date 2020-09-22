6755490e0b7c1ea3eea702844644aa79932bf8f5
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-22 17:21:24 +0300

    1 file modified, 2 added

---
+++ src/app/quality-control-start-screen/quality-control-start-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quality-control-start-screen',
+  templateUrl: './quality-control-start-screen.component.html',
+  styleUrls: ['./quality-control-start-screen.component.css']
+})
+export class QualityControlStartScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
