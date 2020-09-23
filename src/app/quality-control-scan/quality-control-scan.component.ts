b0f6641d1cd807ea0b254fd6787ac6b1950ec4da
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-23 13:02:53 +0300

    1 file modified

--- src/app/quality-control-scan/quality-control-scan.component.ts
+++ src/app/quality-control-scan/quality-control-scan.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit,HostListener } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {LicensePlateService} from "../services/licenseplate.service";
+import {OrderService} from "../services/order.service";
 
 @Component({
   selector: 'app-quality-control-scan',
@@ -9,10 +10,12 @@ import {LicensePlateService} from "../services/licenseplate.service";
 })
 export class QualityControlScanComponent implements OnInit {
   licensePlateId;
-  licensePlate;
+  licensePlate=null;
   scannedValue=[];
+  order=null;
   gettingLicensePlate=false;
-  constructor(private route:ActivatedRoute,private licensePlateService:LicensePlateService) { }
+  gettingOrder=false;
+  constructor(private route:ActivatedRoute,private licensePlateService:LicensePlateService,private orderService:OrderService) { }
   @HostListener('window:keypress',['$event'])keyEvent(event:KeyboardEvent){
     if(event.key==='Enter'){
       this.scanFilter(this.scannedValue.join(""));
@@ -25,11 +28,27 @@ export class QualityControlScanComponent implements OnInit {
     if(scanned.length===18&&scanned[0]=='1'&&scanned[1]=='Z'){
       console.log(scanned+"  this is a tracking number");
       // scan tracking number logic
+      this.trackingNumberLogic(scanned);
     }else{
       console.log(scanned+"   this must be a license plate number")
       // scan license plate logic 
+      this.licensePlateLogic(scanned);
     }
   }
+  trackingNumberLogic(trackingNumber){
+     if(this.order==null){
+        this.getOrder(trackingNumber);
+     }
+  }
+  licensePlateLogic(licensePlate){
+
+  }
+  getOrder(trackingNumber){
+     this.gettingOrder=true;
+
+  }
+
+
   getLP(){
    this.gettingLicensePlate=true;
    this.licensePlateService.getLicensePlate(this.licensePlateId)
