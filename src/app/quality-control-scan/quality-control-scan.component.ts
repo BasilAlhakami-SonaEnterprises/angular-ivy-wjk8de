9d562a9598f6ae7794ad8224087b661056b4aea7
BasilAlhakami-SonaEnterprises <basil@sonaenterprises.com>
2020-09-23 12:28:39 +0300

    1 file modified

--- src/app/quality-control-scan/quality-control-scan.component.ts
+++ src/app/quality-control-scan/quality-control-scan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,HostListener } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {LicensePlateService} from "../services/licenseplate.service";
 
@@ -10,8 +10,26 @@ import {LicensePlateService} from "../services/licenseplate.service";
 export class QualityControlScanComponent implements OnInit {
   licensePlateId;
   licensePlate;
+  scannedValue=[];
   gettingLicensePlate=false;
   constructor(private route:ActivatedRoute,private licensePlateService:LicensePlateService) { }
+  @HostListener('window:keypress',['$event'])keyEvent(event:KeyboardEvent){
+    if(event.key==='Enter'){
+      this.scanFilter(this.scannedValue.join(""));
+      this.scannedValue=[];
+    }else{
+      this.scannedValue.push(event.key);
+    }
+  }
+  scanFilter(scanned){
+    if(scanned.length===18&&scanned[0]=='1'&&scanned[1]=='Z'){
+      console.log(scanned+"  this is a tracking number");
+      // scan tracking number logic
+    }else{
+      console.log(scanned+"   this must be a license plate number")
+      // scan license plate logic 
+    }
+  }
   getLP(){
    this.gettingLicensePlate=true;
    this.licensePlateService.getLicensePlate(this.licensePlateId)
