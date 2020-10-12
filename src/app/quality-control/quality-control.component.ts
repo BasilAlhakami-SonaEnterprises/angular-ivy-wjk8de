950c8fdb0e9b7babcab0e9c0c338e748eb940b05
SudsUsesAWhiteBoard <sudeep@sonaenterprises.com>
2020-10-12 10:03:38 -0700

    2 files modified

--- src/app/quality-control/quality-control.component.ts
+++ src/app/quality-control/quality-control.component.ts
@@ -1,75 +1,77 @@
-import { Component, OnInit,HostListener } from '@angular/core';
-import {LicensePlateService} from "../services/licenseplate.service";
-import {Router} from '@angular/router';
+import { Component, OnInit, HostListener } from "@angular/core";
+import { LicensePlateService } from "../services/licenseplate.service";
+import { StateService } from "../services/state.service";
+import { Router } from "@angular/router";
 @Component({
-  selector: 'app-quality-control',
-  templateUrl: './quality-control.component.html',
-  styleUrls: ['./quality-control.component.css']
+  selector: "app-quality-control",
+  templateUrl: "./quality-control.component.html",
+  styleUrls: ["./quality-control.component.css"]
 })
 export class QualityControlComponent implements OnInit {
-  
-  constructor(private licensePlateService:LicensePlateService,private router:Router) { }
-   creatingAndPrintingLicensePlate=false;
-   licensePlateId;
-   scannedValue=[];
-   gettingLicensePlate=false;
-   error;
-   printerNumber="printer1";
-  ngOnInit() {
-  }
+  constructor(
+    private licensePlateService: LicensePlateService,
+    private stateService: StateService,
+    private router: Router
+  ) {}
+  creatingAndPrintingLicensePlate = false;
+  licensePlateId;
+  scannedValue = [];
+  gettingLicensePlate = false;
+  error;
+  printerNumber = "printer1";
+  ngOnInit() {}
 
-@HostListener('window:keypress',['$event'])keyEvent(event:KeyboardEvent){
-    if(event.key==='Enter'){
-      this.error=null;
+  @HostListener("window:keypress", ["$event"]) keyEvent(event: KeyboardEvent) {
+    if (event.key === "Enter") {
+      this.error = null;
       this.scanFilter(this.scannedValue.join(""));
-      this.scannedValue=[];
-    }else{
+      this.scannedValue = [];
+    } else {
       this.scannedValue.push(event.key);
     }
   }
 
-    scanFilter(scanned){
-    if(scanned.length===0){
+  scanFilter(scanned) {
+    if (scanned.length === 0) {
       console.log("nothing scanned");
-       return;
-    }else{
-      console.log(scanned+"   this must be a license plate number");
-       console.log(scanned);
-       this.licensePlateId=scanned;
-         this.scanLicensePlate();
+      return;
+    } else {
+      console.log(scanned + "   this must be a license plate number");
+      console.log(scanned);
+      this.licensePlateId = scanned;
+      this.scanLicensePlate();
     }
   }
 
-  createAndPrintLicensePlate(){
-    this.creatingAndPrintingLicensePlate=true;
-    this.error=null;
-    this.licensePlateService.createLicensePlate(this.printerNumber).subscribe((data)=>{
-      this.creatingAndPrintingLicensePlate=false;
-      this.licensePlateId=data;
-    },
-    (err)=>{console.log(err)}
-    )
+  createAndPrintLicensePlate() {
+    this.creatingAndPrintingLicensePlate = true;
+    this.error = null;
+    this.licensePlateService.createLicensePlate(this.stateService.printer).subscribe(
+      data => {
+        this.creatingAndPrintingLicensePlate = false;
+        this.licensePlateId = data;
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-  scanLicensePlate(){
-     this.gettingLicensePlate=true;
-   this.licensePlateService.getLicensePlate(this.licensePlateId)
-   .subscribe((data)=>{
-  if(data.status==200){
-   this.router.navigate(['quality-control-scan'],{queryParams:{licensePlateId:this.licensePlateId}})
+  scanLicensePlate() {
+    this.gettingLicensePlate = true;
+    this.licensePlateService.getLicensePlate(this.licensePlateId).subscribe(
+      data => {
+        if (data.status == 200) {
+          this.router.navigate(["quality-control-scan"], {
+            queryParams: { licensePlateId: this.licensePlateId }
+          });
+        }
+        this.gettingLicensePlate = false;
+      },
+      err => {
+        console.log(err);
+        this.error = err.error;
+        this.gettingLicensePlate = false;
+      }
+    );
   }
-    this.gettingLicensePlate=false;
-   },
-   (err)=>{
-     console.log(err);
-     this.error=err.error;
-     this.gettingLicensePlate=false;
-   }
-   );
-      ;
-  }
- 
-  
-
-
-
 }
