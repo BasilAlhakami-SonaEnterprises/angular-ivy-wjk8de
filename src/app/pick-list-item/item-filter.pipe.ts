eb6097fe76ce641cabaa08e69c7e4ff59610b671
SudsUsesAWhiteBoard <sudeep@sonaenterprises.com>
2020-10-15 10:59:04 -0700

    2 files modified

--- src/app/pick-list-item/item-filter.pipe.ts
+++ src/app/pick-list-item/item-filter.pipe.ts
@@ -1,18 +1,15 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'itemFilter'
+  name: "itemFilter"
 })
 export class ItemFilterPipe implements PipeTransform {
-
-    transform(dataItems: any[], filter: Object): any {
-        if (!dataItems || !filter) {
-            return dataItems;
-        }
-        // filter items array, items which match and return true will be
-        // kept, false will be filtered out
-        return dataItems.filter(dataItem => dataItem.Item.indexOf(filter) !== -1);
+  transform(dataItems: any[], filter: Object): any {
+    if (!dataItems || !filter) {
+      return dataItems;
     }
-
-
+    // filter items array, items which match and return true will be
+    // kept, false will be filtered out
+    return dataItems.filter(dataItem => dataItem.Item.indexOf(filter) !== -1);
+  }
 }
