c17523a1b9321992239f01d8583d0bf48ce2ea69
SudsUsesAWhiteBoard <sudeep@sonaenterprises.com>
2020-10-15 10:59:42 -0700

    1 file modified

--- src/app/pick-list-item/pick-list-item.component.ts
+++ src/app/pick-list-item/pick-list-item.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit,HostListener } from "@angular/core";
+import { Component, OnInit, HostListener } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { OrderService } from "../services/order.service";
-import {ItemInfoService} from "../services/item-info.service";
+import { ItemInfoService } from "../services/item-info.service";
 import { Router } from "@angular/router";
 @Component({
-  selector: 'app-pick-list-item',
-  templateUrl: './pick-list-item.component.html',
-  styleUrls: ['./pick-list-item.component.css']
+  selector: "app-pick-list-item",
+  templateUrl: "./pick-list-item.component.html",
+  styleUrls: ["./pick-list-item.component.css"]
 })
 export class PickListItemComponent implements OnInit {
   columnsToDisplay = ["Code", "Qty"];
@@ -14,13 +14,14 @@ export class PickListItemComponent implements OnInit {
   data; // [{Code: "FS374", Qty: 13}, {Code: "DD312", Qty: 20}];
   selection;
   error;
-  loading=false;
-  scannedValue=[];
+  loading = false;
+  scannedValue = [];
+  filterArgs;
   constructor(
     private route: ActivatedRoute,
     private orderService: OrderService,
-    private itemService:ItemInfoService,
-    private router:Router
+    private itemService: ItemInfoService,
+    private router: Router
   ) {}
 
   ngOnInit() {
@@ -51,12 +52,11 @@ export class PickListItemComponent implements OnInit {
       }
     }
   }
-   @HostListener("window:keypress", ["$event"]) keyEvent(event: KeyboardEvent) {
+  @HostListener("window:keypress", ["$event"]) keyEvent(event: KeyboardEvent) {
     if (event.key === "Enter") {
       this.error = null;
-      var scanned=this.scannedValue.join("")
-      if(scanned!=null&&scanned!='')
-      this.getScannedItem(scanned);
+      var scanned = this.scannedValue.join("");
+      if (scanned != null && scanned != "") this.getScannedItem(scanned);
       this.scannedValue = [];
     } else {
       this.scannedValue.push(event.key);
@@ -64,61 +64,62 @@ export class PickListItemComponent implements OnInit {
   }
 
   loadShipsTodayNextDayList() {
-    this.loading=true;
+    this.loading = true;
     this.orderService.getShipsTodayNextDayItemList().subscribe(data => {
-         this.loading=false;
+      this.loading = false;
       console.log(data);
       this.data = data;
     });
   }
 
   loadShipsTodayOtherList() {
-        this.loading=true;
+    this.loading = true;
     this.orderService.getShipsTodayOtherItemList().subscribe(data => {
-        this.loading=false;
+      this.loading = false;
       console.log(data);
       this.data = data;
-      this.data =this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
+      this.data = this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
     });
   }
 
   loadShipsLaterNextDayList() {
-        this.loading=true;
+    this.loading = true;
     this.orderService.getShipsLaterNextDayItemList().subscribe(data => {
-              this.loading=false;
+      this.loading = false;
       console.log(data);
       this.data = data;
-       this.data =this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
+      this.data = this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
     });
   }
 
   loadShipsLaterOtherList() {
-        this.loading=true;
+    this.loading = true;
     this.orderService.getShipsLaterOtherItemList().subscribe(data => {
-              this.loading=false;
+      this.loading = false;
       console.log(data);
       this.data = data;
-       this.data =this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
+      this.data = this.data.sort((a, b) => (a.Item < b.Item ? -1 : 1));
     });
   }
-  
-  getScannedItem(scanned){
-     this.loading=true;
-     this.itemService.getItemInfo(scanned).subscribe(
-       data=>{
-           if (data.status == 200) {
-             console.log(data);
-                this.router.navigate(["item-picking/"+this.selection+"/"+data.body.ItemCode]);
-           }
-            this.loading=false;
-     },
-     err=>{
-           this.error = err.error;
-            this.loading=false;
-     }
-     )
-  }
 
+  getScannedItem(scanned) {
+    this.loading = true;
+    this.itemService.getItemInfo(scanned).subscribe(
+      data => {
+        if (data.status == 200) {
+          console.log(data);
+          this.filterArgs = data.body.ItemCode;
+          console.log(this.filterArgs);
+          //this.router.navigate(["item-picking/"+this.selection+"/"+data.body.ItemCode]);
+        }
+        this.loading = false;
+      },
+      err => {
+        this.error = err.error;
+        this.loading = false;
+      }
+    );
+  }
 }
 
 class Item {
